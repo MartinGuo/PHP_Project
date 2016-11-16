900fb7a912fbf3d45487ae61e01e9bd8dc5b3cdc
guoshaomin <guoshaomin@baijiahulian.com>
2016-11-16 19:48:00 +0800

    delete

--- index.php
+++ index.php
@@ -5,13 +5,7 @@ $ip = '17.207.225.213';
 $ipQuery = new TaobaoIPQuery();
 $info = $ipQuery->getIPInfo($ip);
 var_dump($info);
-*/
+ */
 $str = '智π国际_报名收费订单小时';
 $res = iconv('','UTF-8',$str);
 
-
-echo 'test git revert';
-
-echo 'test reset'
-
-var_dump('test git');die;
